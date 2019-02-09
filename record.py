7dae4545f6d4888cd1a57ed7cfc070204d5bf117
jakobhuss <jakob.huss89@gmail.com>
2019-02-09 14:54:29 +0100

    Adds initial recorder PoC script

---
+++ record.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import signal
+import time
+from gi.repository import GLib
+from pydbus import SessionBus
+
+screencast_bus = SessionBus().get('org.gnome.Shell.Screencast', '/org/gnome/Shell/Screencast')
+file_path = "/home/jake/code/screencast/output.webm"
+frame_rate = int(30)
+show_mouse = True
+recorder_pipeline = "vp8enc min_quantizer=10 max_quantizer=50 cq_level=13 cpu-used=5 deadline=1000000 threads=%T ! queue ! webmmux"
+
+
+def main():
+    screencast_bus.Screencast(file_path, {'framerate': GLib.Variant('i', int(frame_rate)),
+                                          'draw-cursor': GLib.Variant('b', show_mouse),
+                                          'pipeline': GLib.Variant('s', recorder_pipeline)})
+
+    signal.signal(signal.SIGINT, signal_handler)
+    print('Press Ctrl+C to stop video recording.')
+    signal.pause()
+
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C, stopping video recording.')
+    screencast_bus.StopScreencast()
+
+
+main()
