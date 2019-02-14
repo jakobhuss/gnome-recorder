6cc4dfc1e55230373148642ba0968ab97a3e5456
jakobhuss <jakob.huss89@gmail.com>
2019-02-14 20:59:38 +0100

    Adds the ability to list possible recording devices.

--- record.py
+++ record.py
@@ -2,24 +2,33 @@
 import argparse
 import os
 import signal
+import subprocess
+import re
 
 from gi.repository import GLib
 from pydbus import SessionBus
 
 screencast_bus = SessionBus().get('org.gnome.Shell.Screencast', '/org/gnome/Shell/Screencast')
-video_pipeline = 'vp8enc min_quantizer=10 max_quantizer=50 cq_level=13 cpu-used=5 deadline=1000000 threads=%T ! queue ! '
-audio_on_pipeline = 'mux. pulsesrc ! queue ! audioconvert ! vorbisenc ! queue ! mux. webmmux name=mux'
-audio_off_pipeline = 'webmmux'
 
 
 def build_gstreamer_pipeline(audio=False):
+    video_pipeline = 'vp8enc min_quantizer=10 max_quantizer=50 cq_level=13 cpu-used=5 deadline=1000000 threads=%T ! queue ! '
+    audio_on_pipeline = 'mux. pulsesrc ! queue ! audioconvert ! vorbisenc ! queue ! mux. webmmux name=mux'
+    audio_off_pipeline = 'webmmux'
+
     return video_pipeline + (audio_on_pipeline if audio else audio_off_pipeline)
 
 
 def main(args):
-    screencast_bus.Screencast(args.file_path, {'framerate': GLib.Variant('i', int(args.frame_rate)),
-                                               'draw-cursor': GLib.Variant('b', not args.no_cursor),
-                                               'pipeline': GLib.Variant('s', build_gstreamer_pipeline(args.audio))})
+    if args.list_devices:
+        list_audio_devices()
+        return
+
+    screencast_params = {'framerate': GLib.Variant('i', int(args.frame_rate)),
+                         'draw-cursor': GLib.Variant('b', not args.no_cursor),
+                         'pipeline': GLib.Variant('s', build_gstreamer_pipeline(args.audio))}
+
+    screencast_bus.Screencast(args.file_path, screencast_params)
 
     signal.signal(signal.SIGINT, signal_handler)
     print('Press Ctrl+C to stop video recording.')
@@ -29,6 +38,12 @@ def main(args):
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C, stopping video recording.')
     screencast_bus.StopScreencast()
+
+
+def list_audio_devices():
+    pacmd_sources = subprocess.run(['pacmd', 'list-sources'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    pprint(re.findall('name: <(.*)>', pacmd_sources))
+
 
 
 def parse_arguments():
@@ -43,6 +58,8 @@ def parse_arguments():
                              help='Hide mouse cursor')
     args_parser.add_argument('-a', '--audio_on', default=False, dest='audio', action='store_true',
                              help='Records audio from default pulse audio input')
+    args_parser.add_argument('-l', '--list_devices', default=False, dest='list_devices', action='store_true',
+                             help='List possible audio devices for pulse audio')
     args_parser.add_argument('-r', '--frame_rate', type=int, default=30, dest='frame_rate', help='Recording frame rate')
     return args_parser.parse_args()
 
